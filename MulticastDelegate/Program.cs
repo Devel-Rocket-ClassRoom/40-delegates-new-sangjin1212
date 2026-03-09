c2826638a255f7f58af5a1645e245a939d218e61
YunSangJin <sangjin411@gmail.com>
2026-03-09 21:40:58 +0900

    윤상진
    실습완료

--- MulticastDelegate/Program.cs
+++ MulticastDelegate/Program.cs
@@ -1,6 +1,31 @@
 using Microsoft.VisualBasic;
 using System;
+using static System.Net.Mime.MediaTypeNames;
 
-Action<string> PrintOriginal = (msg)=> Console.WriteLine(msg);
-PrintOriginal("Hello World"); 
-Action<string> PrintUpper = (msg)=> Console.WriteLine(msg);
+string msg = "Hello World";
+Action<string> PrintOriginal = (msg) => Console.WriteLine(msg);
+Action<string> PrintUpper = (msg) => Console.WriteLine(msg.ToUpper());
+Action<string> PrintLower = (msg) => Console.WriteLine(msg.ToLower());
+Action<string> PrintLength = (msg) => Console.WriteLine(msg.Length);
+
+Action<string> text = PrintOriginal;
+
+Console.WriteLine("=== 문자열 처리 파이프 라인 ===");
+Console.WriteLine();
+Console.WriteLine("--- 1: 원본 출력만 ---");
+Console.Write($"원본: ");
+text(msg);
+Console.WriteLine();
+Console.WriteLine("--- 2: 대문자 추가 ---");
+text += PrintUpper;
+text(msg);
+Console.WriteLine();
+Console.WriteLine("--- 3: 소문자,길이 추가 ---");
+text += PrintLower;
+text += PrintLength;
+
+text(msg);
+Console.WriteLine();
+Console.WriteLine("--- 4:대문자 제거 ---");
+text -= PrintUpper;
+text(msg);
--- StringProcessor/Program.cs
+++ StringProcessor/Program.cs
@@ -1,4 +1,35 @@
 using System;
+string msg = "Hello World";
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("=== 문자열 가공기 ===");
+Console.WriteLine();
+Console.WriteLine($"원본: {msg}");
+Console.WriteLine();
+
+Console.WriteLine("[대문자 변환]");
+ProcessAndPrint(msg, ToUpperCase);
+
+Console.WriteLine("[소문자 변환]");
+ProcessAndPrint(msg, ToLowerCase);
+
+Console.WriteLine("[괄호 추가]");
+ProcessAndPrint(msg, AddBrackets);
+
+Console.WriteLine("[문자열 뒤집기]");
+ProcessAndPrint(msg, Reverse);
+static string ToUpperCase (string s) =>  s.ToUpper ();
+static string ToLowerCase(string s) => s.ToLower();
+static string AddBrackets(string s) => "[" + s + "]";
+static string Reverse(string s)
+{
+    char[] arr = s.ToCharArray ();
+    Array.Reverse(arr);
+    return new string(arr);
+}
+static void ProcessAndPrint(string input, StringProcessor processor)
+{
+    string result = processor(input);
+    Console.WriteLine($"결과: {result}");
+    Console.WriteLine();
+}
+delegate string StringProcessor(string s);
